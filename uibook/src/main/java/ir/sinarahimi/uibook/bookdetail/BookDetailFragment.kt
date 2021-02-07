cca6682ee0cc76c8ed9666ff87815b81eedc3a11
sina <develop.rahimi95@gmail.com>
2021-02-07 21:27:55 +0330

    Add BookDetailFragment.kt Add LibraryFragment.kt

--- uibook/src/main/java/ir/sinarahimi/uibook/bookdetail/BookDetailFragment.kt
+++ uibook/src/main/java/ir/sinarahimi/uibook/bookdetail/BookDetailFragment.kt
@@ -1,25 +1,13 @@
 package ir.sinarahimi.uibook.bookdetail
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ir.sinarahimi.uibook.R
 
-
-class BookDetailFragment : Fragment() {
+class BookDetailFragment : Fragment(R.layout.fragment_book_detail) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_detail, container, false)
     }
 
     companion object {
