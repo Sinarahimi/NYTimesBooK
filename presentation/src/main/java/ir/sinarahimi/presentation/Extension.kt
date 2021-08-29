3f704bc8b5f69cf0187d47022fc97cc44890ecd4
sina <develop.rahimi95@gmail.com>
2021-08-29 12:24:42 +0430

    [Modified] reformat code

--- presentation/src/main/java/ir/sinarahimi/presentation/Extension.kt
+++ presentation/src/main/java/ir/sinarahimi/presentation/Extension.kt
@@ -3,6 +3,5 @@ package ir.sinarahimi.presentation
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
-
 fun View.showSnackbar(message: String) =
     Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
